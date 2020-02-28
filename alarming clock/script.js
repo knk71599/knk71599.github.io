4b1ff6e3175faa75f5b9ce8331cfb8236e39c3e6
Kristen Knoblauch <43049063+knk71599@users.noreply.github.com>
2020-02-27 20:46:01 -0500

    Add files via upload

--- alarming clock/script.js
+++ alarming clock/script.js
@@ -1,63 +1,13 @@
 $(document).ready(function(){
+
+	$("audio")[0].play();
   
   setTimeout(function() {
     $(".moon").addClass("alarm");
+    $(".light").addClass("blinking1")
+    $(".med").addClass("blinking2")
+    $(".dark").addClass("blinking3")
   }, 6000);
 
-setTimeout(function() {
-  $(".light").addClass("noopacity");
-  }, 6000);
-setTimeout(function() {
-  $(".noopacity").addClass("light");
-  }, 7000);
-setTimeout(function() {
-  $(".light").addClass("noopacity");
-  }, 8000);
-setTimeout(function() {
-  $(".noopacity").addClass("light");
-  }, 9000);
-setTimeout(function() {
-  $(".light").addClass("noopacity");
-  }, 10000);
-setTimeout(function() {
-  $(".noopacity").addClass("light");
-  }, 11000);
-  
-setTimeout(function() {
-  $(".med").addClass("noopacity");
-  }, 7000);
-setTimeout(function() {
-  $(".noopacity").addClass("med");
-  }, 8000);
-setTimeout(function() {
-  $(".med").addClass("noopacity");
-  }, 9000);
-setTimeout(function() {
-  $(".noopacity").addClass("med");
-  }, 10000);
-setTimeout(function() {
-  $(".med").addClass("noopacity");
-  }, 11000);
-setTimeout(function() {
-  $(".noopacity").addClass("med");
-  }, 12000);
 
-setTimeout(function() {
-  $(".dark").addClass("noopacity");
-  }, 6000);
-setTimeout(function() {
-  $(".noopacity").addClass("dark");
-  }, 7000);
-setTimeout(function() {
-  $(".dark").addClass("noopacity");
-  }, 8000);
-setTimeout(function() {
-  $(".noopacity").addClass("dark");
-  }, 9000);
-setTimeout(function() {
-  $(".dark").addClass("noopacity");
-  }, 10000);
-setTimeout(function() {
-  $(".noopacity").addClass("dark");
-  }, 11000);
 });
