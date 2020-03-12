cea8d920faf9cd486edd4dd179bace4a178a89e6
Kristen Knoblauch <43049063+knk71599@users.noreply.github.com>
2020-03-12 15:43:32 -0400

    Add files via upload

--- alarming clock/script.js
+++ alarming clock/script.js
@@ -1,13 +1,17 @@
 $(document).ready(function(){
 
-	$("audio")[0].play();
   
   setTimeout(function() {
     $(".moon").addClass("alarm");
     $(".light").addClass("blinking1")
-    $(".med").addClass("blinking2")
-    $(".dark").addClass("blinking3")
-  }, 6000);
+  }, 15000);
 
+setTimeout(function() {
+    $(".med").addClass("blinking2")
+  }, 15300);
+
+setTimeout(function() {
+    $(".dark").addClass("blinking3")
+  }, 15600);
 
 });
