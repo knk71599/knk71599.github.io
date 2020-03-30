e443700cf73f18d1b611b667e42bbe0f5a92f980
Kristen Knoblauch <43049063+knk71599@users.noreply.github.com>
2020-03-30 15:02:09 -0400

    Add files via upload

--- sirglob3/script.js
+++ sirglob3/script.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
   var mugen = new MUGEN();
-  mugen.initialize({titleScreenImageURL: "./img/title-screen.png", actionRadius: 115, timeBetweenNPCs: 3400, npcCollisionRadius: 100, actionDuration:500, jumpHeight: 225});
+  mugen.initialize({titleScreenImageURL: "./img/title-screen.png", actionRadius: 115, timeBetweenNPCs: 3400, npcCollisionRadius: 100, actionDuration:500, jumpHeight: 225, removeNPCAfterInteractedDelay: 3000});
 });
